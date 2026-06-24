ed76e661d19994e34be738e9397debcfb3c7255d
thefearlesscoder <thefunnelvkd@gmail.com>
2026-06-24 15:24:20 +0530

    Add agent manager and refund processing tools

---
+++ agent_manager.js
@@ -0,0 +1,65 @@
+import 'dotenv/config';
+import {Agent, tool, run} from '@openai/agents';
+import {z} from 'zod';
+import fs from 'node:fs/promises';
+
+//tool
+const fetchAvailablePlans = tool({
+    name: 'fetch_avaible_plans',
+    description:'fetches the available plans for internet',
+    parameters: z.object({}),
+    execute: async function () {
+        return [
+      { plan_id: '1', price_inr: 399, speed: '30MB/s' },
+      { plan_id: '2', price_inr: 999, speed: '100MB/s' },
+      { plan_id: '3', price_inr: 1499, speed: '200MB/s' },
+    ]; 
+    },
+})
+
+//tool
+const processRefund = tool({
+    name: 'process_refund',
+    description: 'this tool processes the refund for a customer',
+    parameters: z.object({
+        customer_id: z.string().describe('id of the customer'),
+        reason: z.string().describe('reason for the refund'),
+    }),
+    execute: async function ({ customerId, reason }) {
+    await fs.appendFile(
+      './refunds.txt',
+      `Refund for Customer having ID ${customerId} for ${reason}`,
+      'utf-8'
+    );
+    return { refundIssued: true };
+  },
+})
+
+const refundAgent = new Agent({
+    name: 'refund_agent',
+    instructions: `
+    You are an expert in issueing refunds to the customers
+    `,
+    tools: [processRefund]
+})
+
+const salesAgent = new Agent({
+    name: 'sales_agent',
+    instructions: `
+    You are an expert sales agent for an internet braodband company. Talk to them
+    and help them with what they need.
+    `,
+    tools: [fetchAvailablePlans, refundAgent.asTool({
+        toolName: 'refunt_expert',
+        toolDescription: 'handles refund questions and request'
+
+    })],
+})
+
+async function runAgent(query = '') {
+    const result = await run(salesAgent, query);
+    console.log(result.finalOutput);
+}
+
+// runAgent(`Hey There, i want to know the available plans`);
+runAgent(`Hey i had a plan 399. I want a refund now. My customer id is cust123`);
